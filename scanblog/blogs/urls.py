5b59aeec40df98b8fb106fa90917e3325b978a5a
Charlie DeTar <cfd@media.mit.edu>
2012-01-23 17:50:04 -0500

    fixing feed urls

--- scanblog/blogs/urls.py
+++ scanblog/blogs/urls.py
@@ -2,15 +2,15 @@ from django.conf.urls.defaults import *
 
 post_id = "(?P<post_id>\d+)"
 author_id = "(?P<author_id>\d+)"
-slug = "(?P<slug>[^/]*)"
+slug = "(?P<slug>[-a-z0-9]+)"
 revision_id = "(?P<revision_id>\d+)"
 
 urlpatterns = patterns('blogs.views',
     # Post lists
     url(r'^blogs/$', 'all_posts_list', name='blogs.home'),
     url(r'^blogs/tag/(?P<tag>.+)/$', 'tagged_post_list', name='blogs.tagged_posts'),
-    url(r'^blogs/{0}/{1}/?$'.format(author_id, slug), 'author_post_list', name='blogs.blog_show'),
-    url(r'^blogs/{0}/$'.format(slug), 'org_post_list', name='blogs.org_post_list'),
+    url(r'^blogs/{0}/{1}?/?$'.format(author_id, slug), 'author_post_list', name='blogs.blog_show'),
+
     url(r'^blogs/feed/$', 'all_posts_feed', name='blogs.all_posts_feed'),
     url(r'^blogs/feed/{0}/$'.format(author_id), 'author_post_feed', name='blogs.blog_feed'),
     url(r'^blogs/feed/comments/$', 'all_comments_feed', name='blogs.all_comments_feed'),
@@ -20,10 +20,13 @@ urlpatterns = patterns('blogs.views',
         name='blogs.org_feed_unfiltered'),
     url(r'^blogs/tag/(?P<tag>.+)/feed$', 'tagged_post_feed', name='blogs.tagged_posts_feed'),
 
+    # Org has to come after feed, so that it doesn't consume 'feed' and 'tag' as slugs.
+    url(r'^blogs/{0}/$'.format(slug), 'org_post_list', name='blogs.org_post_list'),
+
     url(r'^blogs/{0}/{1}/feed/$'.format(author_id, slug), 'legacy_author_post_feed'),
 
     # Individual posts
-    url(r"^posts/{0}/{1}/?$".format(post_id, slug), 'post_detail', name='blogs.post_show'),
+    url(r"^posts/{0}/{1}?/?$".format(post_id, slug), 'post_detail', name='blogs.post_show'),
     url(r"^posts/commentfeed/{0}$".format(post_id), 'post_comments_feed', name='blogs.post_comments_feed'),
     url(r"^posts/tag/{0}$".format(post_id), 'save_tags', name='blogs.tag_post'),
     url(r"^posts/taglist$", 'post_tag_list', name='blogs.post_tag_list'),
