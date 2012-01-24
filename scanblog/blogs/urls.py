d7470f3de73274f6572a111633800aabde5c7b56
cfd <cfd@media.mit.edu>
2012-01-24 16:32:12 -0500

    fixing url redirection bug

--- scanblog/blogs/urls.py
+++ scanblog/blogs/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls.defaults import *
 
 post_id = "(?P<post_id>\d+)"
 author_id = "(?P<author_id>\d+)"
-slug = "(?P<slug>[-a-z0-9]+)"
+slug = "(?P<slug>[-a-z0-9]*)"
 revision_id = "(?P<revision_id>\d+)"
 
 urlpatterns = patterns('blogs.views',
     # Post lists
     url(r'^blogs/$', 'all_posts_list', name='blogs.home'),
     url(r'^blogs/tag/(?P<tag>.+)/$', 'tagged_post_list', name='blogs.tagged_posts'),
-    url(r'^blogs/{0}/{1}?/?$'.format(author_id, slug), 'author_post_list', name='blogs.blog_show'),
+    url(r'^blogs/{0}/{1}/?$'.format(author_id, slug), 'author_post_list', name='blogs.blog_show'),
 
     url(r'^blogs/feed/$', 'all_posts_feed', name='blogs.all_posts_feed'),
     url(r'^blogs/feed/{0}/$'.format(author_id), 'author_post_feed', name='blogs.blog_feed'),
@@ -26,7 +26,7 @@ urlpatterns = patterns('blogs.views',
     url(r'^blogs/{0}/{1}/feed/$'.format(author_id, slug), 'legacy_author_post_feed'),
 
     # Individual posts
-    url(r"^posts/{0}/{1}?/?$".format(post_id, slug), 'post_detail', name='blogs.post_show'),
+    url(r"^posts/{0}/{1}/?$".format(post_id, slug), 'post_detail', name='blogs.post_show'),
     url(r"^posts/commentfeed/{0}$".format(post_id), 'post_comments_feed', name='blogs.post_comments_feed'),
     url(r"^posts/tag/{0}$".format(post_id), 'save_tags', name='blogs.tag_post'),
     url(r"^posts/taglist$", 'post_tag_list', name='blogs.post_tag_list'),
