9a36d46824ecb95fa0d7c177a8f8872c8f310d44
Charlie DeTar <cfd@media.mit.edu>
2012-04-03 12:00:53 -0400

    Fixing process killing in fromage.py

--- scanblog/fromage.py
+++ scanblog/fromage.py
@@ -19,36 +19,48 @@ if __name__ == "__main__":
         ports = sys.argv[1:]
         runtests = False
 
-    autocompile = subprocess.Popen([MANAGE_PATH, "autocompile"])
-    djcelery    = subprocess.Popen([MANAGE_PATH, "celeryd", "--verbosity=2"])
-    runserver   = subprocess.Popen([MANAGE_PATH, "runserver"] + ports,
-                               stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-                               
-    return_code = 0
-
-    if runtests:
-        try:
-            collectstatic = subprocess.Popen([MANAGE_PATH, "collectstatic", "--noinput"])
-            collectstatic.communicate()
-            unittests = subprocess.Popen(
-                    [MANAGE_PATH, "test", "btb", "about", "profiles", 
-                        "annotations", "scanning", "subscriptions", "blogs"])
-            unittests.communicate()
-            assert unittests.returncode == 0
-            integration = subprocess.Popen([MANAGE_PATH, "harvest"])
-            integration.communicate()
-            assert integration.returncode == 0
-        except AssertionError:
-            return_code = 1
-            print "Tests failed!"
-        else:
-            return_code = 0
-            print "Tests passed."
     try:
-        runserver.communicate()
-    except KeyboardInterrupt:
-        pass
-    runserver.kill()
-    autocompile.kill()
-    djcelery.kill()
-    sys.exit(return_code)
+        autocompile = subprocess.Popen(
+                [MANAGE_PATH, "autocompile"],
+                preexec_fn=os.setsid)
+        djcelery    = subprocess.Popen(
+                [MANAGE_PATH, "celeryd", "--verbosity=2"],
+                preexec_fn=os.setsid)
+        runserver   = subprocess.Popen(
+                [MANAGE_PATH, "runserver"] + ports,
+                stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                preexec_fn=os.setsid)
+                                   
+        return_code = 0
+
+        if runtests:
+            try:
+                collectstatic = subprocess.Popen(
+                        [MANAGE_PATH, "collectstatic", "--noinput"])
+                collectstatic.communicate()
+                unittests = subprocess.Popen(
+                        [MANAGE_PATH, "test", 
+                            "btb", "about", "profiles", 
+                            "annotations", "scanning", 
+                            "subscriptions", "blogs"])
+                unittests.communicate()
+                assert unittests.returncode == 0
+                integration = subprocess.Popen([MANAGE_PATH, "harvest"])
+                integration.communicate()
+                assert integration.returncode == 0
+            except AssertionError:
+                return_code = 1
+                print "Tests failed!"
+            else:
+                return_code = 0
+                print "Tests passed."
+        try:
+            runserver.communicate()
+        except KeyboardInterrupt:
+            pass
+    finally:
+        # Use os.killpg to ensure that shell-launched process groups are all
+        # killed, not just the launching process.
+        for proc in [runserver, autocompile, djcelery]:
+            os.killpg(proc.pid, signal.SIGTERM)
+        sys.exit(return_code)
