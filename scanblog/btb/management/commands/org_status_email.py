f5d6834e877f97f5801deaf3d7abde8543018aea
Charlie DeTar <cfd@media.mit.edu>
2012-07-10 18:41:34 -0400

    Exclude unmanaged scans from org status email

--- scanblog/btb/management/commands/org_status_email.py
+++ scanblog/btb/management/commands/org_status_email.py
@@ -50,7 +50,9 @@ def send_org_mail(org):
 
     # Scans.
     scans = Scan.objects.org_filter(org_user).filter(
-            processing_complete=False
+            processing_complete=False,
+        ).exclude(
+            profile__author__managed=False
         ).order_by('created')
     finished_scans =  Scan.objects.org_filter(org_user).filter(
             processing_complete=True
