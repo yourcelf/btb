bdba6b40520044fbdd9abc332c39e894309958f1
Charlie DeTar <cfd@media.mit.edu>
2012-10-16 10:43:46 -0400

    Add incoming tests

--- scanblog/btb/tests/live/__init__.py
+++ scanblog/btb/tests/live/__init__.py
@@ -3,13 +3,14 @@ from .comment_tests import TestComments
 from .flag_tests import TestFlags
 from .frontend_tests import TestFrontend
 from .transcription_tests import TestTranscriptions
+from .post_from_web_tests import TestPostFromWeb
 from .mod_upload_tests import TestModUploads
 from .mod_scan_tests import TestModScans
-from .post_from_web_tests import TestPostFromWeb
 from .mod_users_tests import TestModUsers
+from .mod_incoming_tests import TestModIncoming
 
 __all__ = [
     'TestAccounts', 'TestComments', 'TestFlags', 'TestFrontend',
-    'TestTranscriptions', 'TestModUploads', 'TestModScans',
-    'TestPostFromWeb', 'TestModUsers',
+    'TestTranscriptions', 'TestPostFromWeb', 
+    'TestModUploads', 'TestModScans', 'TestModUsers', 'TestModIncoming',
 ]
