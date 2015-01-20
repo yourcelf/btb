22dd35e64232407ab11400a6adbfcf5a185d1760
Charlie DeTar <cfd@media.mit.edu>
2015-01-20 08:53:49 -0700

    Improve reply code admin

--- scanblog/annotations/admin.py
+++ scanblog/annotations/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from annotations.models import Note, Tag, ReplyCode
 
-for model in (Note,Tag,ReplyCode):
+class ReplyCodeAdmin(admin.ModelAdmin):
+    search_fields = ['code']
+    list_filter = ['document__author']
+admin.site.register(ReplyCode, ReplyCodeAdmin)
+
+for model in (Note,Tag):
     admin.site.register(model)
