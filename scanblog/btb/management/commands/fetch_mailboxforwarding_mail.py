8c101da101fa370f13a0dbb440dce7993cbd2497
Charlie DeTar <cfd@media.mit.edu>
2015-11-19 10:44:34 -0700

    Update mbf import; prepend envelopes

--- scanblog/btb/management/commands/fetch_mailboxforwarding_mail.py
+++ scanblog/btb/management/commands/fetch_mailboxforwarding_mail.py
@@ -5,6 +5,9 @@ import time
 import json
 import tempfile
 import requests
+from cStringIO import StringIO
+from pyPdf import PdfFileWriter, PdfFileReader
+from PIL import Image
 
 # Work around InseurePlatformWarning:
 # https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning
@@ -43,10 +46,12 @@ class Command(BaseCommand):
             "loginsubmit.x": "0",
             "loginsubmit.y": "0"
         })
-        # This is a dslightly dirty hack -- we're matching a javascript data
+        # This is a slightly dirty hack -- we're matching a javascript data
         # structure with a regex, converting the quotes to doubles so it resembles
         # JSON, and then loading it as JSON.  This may prove brittle.
         match = re.search(r"Ext\.grid\.dummyData = (\[.*\]\]);", res.text, re.DOTALL)
+        if not match:
+            raise Exception("Can't find data. Are login creds correct?")
         text = match.group(1)
         text = text.replace('"', '\\"')
         text = text.replace("'", '"')
@@ -54,38 +59,72 @@ class Command(BaseCommand):
 
         scans = {}
         packages = {}
-        for a,b,date,c,kind,status,dl in data:
+        for checkbox, date, envelope, type_status, dl in data:
             match = re.search("pdfview.php\?id=(\d+)", dl)
             if match:
                 id_ = match.group(1)
             else:
-                id_ = None
+                raise Exception("Can't find ID")
 
-            obj = {"date": date, "kind": kind, "status": status}
+            details = {}
 
-            if status == "Scanned":
-                scans[id_] = obj
-            else:
-                packages[id_] = obj
+            match = re.search("src=\"([^\"]+)\"", envelope)
+            if not match:
+                raise Exception("Can't match envelope image")
+            details['envelope'] = match.group(1)
+
+            match = re.search("Type: <b>([^<]+)</b>.*Status: <b>([^<]+)</b>", type_status)
+            if not match:
+                raise Exception("Can't match type/status")
+            details['kind'] = match.group(1)
+            details['status'] = match.group(2)
+
+            if details['status'] == "Scanned":
+                scans[id_] = details
+            elif details['kind'] != "Letter":
+                packages[id_] = details
 
         uploader = User.objects.get(username="uploader")
         org = Organization.objects.get(pk=1) #TODO: generalize this? 
 
         new_count = 0
-        for id_,details in scans.iteritems():
+        for id_, details in scans.iteritems():
             source_id = "mailboxforwarding.com-{}".format(id_)
             if Scan.objects.filter(source_id=source_id).exists():
                 continue
             new_count += 1
 
-            print "Downloading", source_id
+            print "Downloading pdf", source_id
+            res = sess.get("{}manage/pdfview.php?id={}".format(base_url, id_))
+            in_pdf_fh = StringIO()
+            in_pdf_fh.write(res.content)
+            in_pdf_fh.seek(0)
+            reader = PdfFileReader(in_pdf_fh)
+
+            print "Downloading envelope", details['envelope']
+            res = sess.get(details['envelope'])
+            in_envelope_fh = StringIO()
+            in_envelope_fh.write(res.content)
+            in_envelope_fh.seek(0)
+            img = Image.open(in_envelope_fh)
+            out_envelope_fh = StringIO()
+            img.save(out_envelope_fh, "pdf")
+            envelope_reader = PdfFileReader(out_envelope_fh)
+
+            writer = PdfFileWriter()
+            writer.addPage(envelope_reader.getPage(0))
+            for page in range(reader.getNumPages()):
+                writer.addPage(reader.getPage(page))
 
             with tempfile.NamedTemporaryFile(suffix=".pdf", delete=False) as fh:
-                res = sess.get("{}manage/pdfview.php?id={}".format(base_url, id_))
-                fh.write(res.content)
-                name = fh.name
+                writer.write(fh)
+                dest_pdf_name = fh.name
 
-            path = tasks.move_scan_file(filename=name)
+            in_envelope_fh.close()
+            out_envelope_fh.close()
+            in_pdf_fh.close()
+
+            path = tasks.move_scan_file(filename=dest_pdf_name)
             scan = Scan.objects.create(
                 uploader=uploader,
                 pdf=os.path.relpath(path, settings.MEDIA_ROOT),
@@ -95,10 +134,12 @@ class Command(BaseCommand):
             )
             tasks.split_scan(scan=scan)
 
+            return
+
         if packages:
             print "Manual action needed on the following at " \
                   "https://www.mailboxforwarding.com/:"
-            for id_,details in packages.iteritems():
+            for id_, details in packages.iteritems():
                 new_count += 1
                 print details
         print "Examined {} letters, {} new.".format(len(data), new_count)
